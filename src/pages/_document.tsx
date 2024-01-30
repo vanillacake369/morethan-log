c2568976eff2ad33f9f58744b438ed67b6918e60
LimJihoon (Tony) <lonelynight1026@gmail.com>
2024-01-30 16:45:33 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="192x192"
-            href="/apple-touch-icon.png"
+            href="/apple-icon-192x192.png"
           ></link>
           <link
             rel="alternate"
